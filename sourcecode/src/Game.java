13306fe743ab4c61f52c173194cb3e209cb044d6
Phuc Nguyen <phucnguynn1301@gmail.com>
2026-04-26 14:22:40 +0700

    Implement core logic and attributes for Game class

--- sourcecode/src/Game.java
+++ sourcecode/src/Game.java
@@ -1,13 +1,51 @@
 public class Game {
-    //TODO 1: Create all attributes
+    // Attributes
+    private Board board;
+    private Player[] players;
+    private int currentPlayer;
+    private boolean isFinished;
 
+    // Constructor
+    public Game(Player player1Name, Player player2Name){
+        this.players = new Player[]{player1Name, player2Name};
+        this.board = new Board();
+        this.currentPlayer = 0;
+        this.isFinished = false;
+    }
+
+    // Logic to calculate and add score after a move ends
     public int handleCapture(){
-        //TODO 2: implement, return the total gem value captured
+        int totalCaptured = 0;
+        // TODO: Call board.captureGems(stopIndex) to get captured gems
+        // Then add totalCaptured to players[currentPlayer].addScore()
+        return totalCaptured;
     }
 
-    public void switchPLayer(){
-        //TODO 3: simply 1 - currentPlayer
+    // Switch turn between player1 (0) and player2 (1)
+    public void switchPlayer(){
+        this.currentPlayer = 1 - this.currentPlayer;
     }
 
-    //TODO 4: implement all the get method
+    // Update game status based on board state
+    public void updateGameState(){
+        this.isFinished = this.board.checkEnding();
+    }
+
+    // Getters
+    public int getCurrentPlayer(){
+        return this.currentPlayer;
+    }
+
+    public Board getBoard(){
+        return this.board;
+    }
+
+    public boolean isFinished(){
+        return this.isFinished;
+    }
+
+    // Return the current Player object
+    public Player getActivePlayerObject(){
+        return this.players[this.currentPlayer];
+    }
 }
