14666a451d7d3937f1ea4a30df9bdb2e87e0f242
nhatnam2k6 <nam.nn2417174@sis.hust.edu.vn>
2026-05-04 09:22:20 +0700

    Draft player

--- sourcecode/src/Player.java
+++ sourcecode/src/Player.java
@@ -1,3 +1,40 @@
 public class Player {
-    //TODO: add attributes and methods
+    private String name;
+    private double score;
+    private boolean isPlaying;
+    public Player(String name, double score) {
+        this.name = name;
+        this.score = 0;
+        this.isPlaying = false;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public double getScore() {
+        return score;
+    }
+
+    public void resetScore(){
+        this.score = 0;
+    }
+
+    public boolean isPlaying() {
+        return isPlaying;
+    }
+
+    public void setPlaying(boolean isPlaying) {
+        this.isPlaying = isPlaying;
+    }
+
+    public void addScore(int points){
+        if(points < 0){
+            this.score += points;
+        }
+    }
+    @Override
+    public String toString() {
+        return "Player{" + "name='" + name + '\'' + ", score=" + score + ", isPlaying=" + isPlaying + '}';
+    }
 }
