3763752ccb2bcebf5f8feb3b1514b8931584263b
unknown <tts423815@gmail.com>
2026-05-06 08:41:43 +0700

    Main Game avatar frame added

--- sourcecode/src/Controller/Controller.java
+++ sourcecode/src/Controller/Controller.java
@@ -18,13 +18,7 @@ public class Controller {
     private Scene scene;
     private Parent root;
 
-    @FXML
-    private ImageView playerOneAvatar;
-
-    @FXML
-    public void initialize() {
-        playerOneAvatar.setImage(new Image("/image/avatar/Pattrick_2.png"));
-    }
+  
 
     
 
