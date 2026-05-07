269df2e1594cb6962c2677b75e56b20c4d34f2e3
nhatnam2k6 <nam.nn2417174@sis.hust.edu.vn>
2026-05-07 10:34:46 +0700

    Editing Controller.java

--- sourcecode/src/Controller.java
+++ sourcecode/src/Controller.java
@@ -1,5 +1,25 @@
-public class Controller {
-    public static void main(String[] args) throws Exception {
-        
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.control.Button;
+import javafx.scene.layout.AnchorPane;
+
+public class Controller extends AnchorPane {
+    @FXML
+    private Button btn;
+    public Controller() {
+        FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("/sourcecode/src/design/Player_interact.fxml"));
+        fxmlLoader.setRoot(this);
+        fxmlLoader.setController(this);
+        try {
+            fxmlLoader.load();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+    public static void main(String[] args) {
+        	Controller controller = new Controller();
+        	controller.btn.setOnAction(e -> {
+                System.out.println("Button clicked!");
+            });
     }
 }
