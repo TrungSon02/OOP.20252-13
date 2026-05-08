a620d950871b1a0a79642c54b6d8d850db4caa14
Phuc Nguyen <phucnguynn1301@gmail.com>
2026-05-08 11:34:37 +0700

    feat: add BaseController for scene navigation

--- sourcecode/src/Controller/BaseController.java
+++ sourcecode/src/Controller/BaseController.java
@@ -1,32 +1,64 @@
 package Controller;
 
+import javafx.fxml.FXML;
+import javafx.application.Platform;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
+import java.io.IOException;
+import java.util.Objects;
 
 public class BaseController {
     private Stage stage;
     private Scene scene;
     private Parent root;
 
-    public void loadScene(){
+    public void loadScene(String fxmlPath, ActionEvent event){
         //PHUC
         //TODO: Get a String input param (the path to a fxml file). Load the scene + Load the css (same command as in App.java)
+        try{
+            
+            root = FXMLLoader.load(Objects.requireNonNull(getClass().getResource(fxmlPath)));
+
+            stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+
+            scene = new Scene(root);
+
+            String css = Objects.requireNonNull(getClass().getResource("/css/application.css")).toExternalForm();
+            scene.getStylesheets().add(css);
+
+            stage.setScene(scene);
+            stage.show();
+
+        } catch (IOException e){
+            System.err.println("Fail to find FXML file: ");
+            e.printStackTrace();
+        }
     }
 
-    public void goHome(){
+    @FXML
+    public void goHome(ActionEvent event){
         //PHUC
         //TODO: 1 line: Use the loadScene method above to load the menu.fxml scene
+        loadScene("/fxml/menu.fxml", event);
     }
 
-    public void goPlay(){
+    @FXML
+    public void goPlay(ActionEvent event){
         //PHUC
         //TODO: 1 line: Use the loadScene method above to load the main_game.fxml scene
+        loadScene("/fxml/main_game.fxml", event);
     }
 
+    @FXML
     public void exit(){
         //PHUC
         //TODO: Exit the game (Like alt f4). (Watch the Code Bro YT vid for the sample code for exiting game)
+        Platform.exit();
+        System.exit(0);
     }
 
 }
