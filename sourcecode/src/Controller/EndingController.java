9f26cec10fcb7dd17110667e3b6f1e3b9270cd38
Phuc Nguyen <phucnguynn1301@gmail.com>
2026-05-08 11:45:30 +0700

    feat: implement minimalist Ending screen with winner display

--- sourcecode/src/Controller/EndingController.java
+++ sourcecode/src/Controller/EndingController.java
@@ -1,11 +1,38 @@
 package Controller;
 
 import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
 
 public class EndingController extends BaseController{
+    @FXML private Label winnerNameLabel;
+    @FXML private Label winnerScoreLabel;
+    @FXML private ImageView winnerAvatarView;
+
     @FXML
     public void initialize(){
         //PHUC
         //TODO: Get the input param: player name and player avatar then update the text label + image to show that
+        System.out.println("Final results screen initialized.");
+    }
+
+    public void displayWinner(String name, int score, String avatarPath) {
+        if (winnerNameLabel != null) {
+            winnerNameLabel.setText("WINNER: " + name);
+        }
+
+        if (winnerScoreLabel != null) {
+            winnerScoreLabel.setText("Final Score: " + score);
+        }
+
+        try {
+            if (winnerAvatarView != null && avatarPath != null) {
+                Image avatarImage = new Image(getClass().getResourceAsStream(avatarPath));
+                winnerAvatarView.setImage(avatarImage);
+            }
+        } catch (Exception e) {
+            System.err.println("Error: Failed to load winner avatar from " + avatarPath);
+        }
     }
 }
