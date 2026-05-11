21473a81b77ddebbe557280c7d02454c4cfb9ebc
unknown <tts423815@gmail.com>
2026-05-11 08:15:55 +0700

    Player class merged

--- sourcecode/src/Model/Game.java
+++ sourcecode/src/Model/Game.java
@@ -1,6 +1,10 @@
 package Model;
 public class Game {
     //TODO 1: Create all attributes
+    private Board board;
+    private Player[] players;
+    private int currentPlayer;
+    private boolean isFinished;
 
     public int handleCapture(){
         //TODO 2: implement, return the total gem value captured
