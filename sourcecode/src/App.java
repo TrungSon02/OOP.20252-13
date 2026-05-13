fa8cd0c5d468b80b801e1e7a83098e68fcdad28d
unknown <tts423815@gmail.com>
2026-05-13 18:55:23 +0700

    Board class merged and minor fix

--- sourcecode/src/App.java
+++ sourcecode/src/App.java
@@ -1,12 +1,10 @@
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
-import javafx.scene.Group;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.image.Image;
 import javafx.scene.paint.Color;
 import javafx.scene.text.Font;
-import javafx.scene.text.Text;
 import javafx.stage.Stage;
 
 
--- sourcecode/src/Controller/MainGameController.java
+++ sourcecode/src/Controller/MainGameController.java
@@ -2,7 +2,6 @@ package Controller;
 
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
-import javafx.scene.input.MouseEvent;
 
 
 public class MainGameController extends BaseController{
